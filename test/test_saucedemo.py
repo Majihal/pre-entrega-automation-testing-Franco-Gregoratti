dcd55c2a443d1b36cfef1d8ccf1f61bf4798cd00
Majihal <frangregos@gmail.com>
2025-10-19 15:15:31 -0300

    que hacer para la entrega, readme

---
+++ test/test_saucedemo.py
@@ -0,0 +1,38 @@
+import pytest
+
+
+
+
+
+
+@pytest.fixture
+def driver():
+    # Configuracion para consultar a selenium web driver
+
+
+
+def test_login():
+    # logeo de usuario con username y password
+    # Click al boton de login
+
+    #rediriga a la pagina inventario
+    #verificar el titulo de la pagina
+
+
+def test_catalogo():
+    # logueo de usuario con username y pass
+    # Click al boton de login
+
+    #podamos verificar el titulo pero del html
+    #comprobar si existen productos visibles en la pagina web (len())
+    #verificar elementos importantes de la pagina
+
+
+def test_carrito():  
+
+    # logueo de usuario con username y pass
+    # Click al boton de login
+    # llevarme a la pagina de carrito de compras
+    #incremento de carrito al agregar un producto
+
+    #comrpobar que el carrito aparezca el producto correctamente. 
